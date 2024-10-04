c0ff644727e640e237d571caff17edcede27909b
therecluse26 <therecluse26@protonmail.com>
2024-10-04 13:56:26 -0400

    Updated logo and favicon

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,8 @@
-import { appConfig } from "@/config/app";
-import { Icons } from "./icons";
-
 export function Logo() {
   return (
     <>
-      {/* <Icons.logo className="h-6 w-6" /> */}
       <svg width="100%" height="35">
-        <image href="soundslice-logo-white.svg" width="100%" height="35" />
+        <image href="soundslice-logo.svg" width="100%" height="35" />
       </svg>
     </>
   );
