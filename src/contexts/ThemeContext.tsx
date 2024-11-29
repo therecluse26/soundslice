8b2a4f9239e64c71a63b2b34012aceb67613c7da
therecluse26 <therecluse26@protonmail.com>
2024-11-29 16:29:15 -0500

    Removed possibility of light theme, no system theme reliance

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -30,20 +30,8 @@ export function ThemeProvider({
 
     useEffect(() => {
         const root = window.document.documentElement
-
         root.classList.remove("light", "dark")
-
-        if (theme === "system") {
-            const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-                .matches
-                ? "dark"
-                : "light"
-
-            root.classList.add(systemTheme)
-            return
-        }
-
-        root.classList.add(theme)
+        root.classList.add("dark")
     }, [theme])
 
     return (
